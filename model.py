d88b49ed67e791c32a12d025bc2a027ac3abf1fc
nantsing <nanqing_zhao@sjtu.edu.cn>
2023-03-28 22:19:17 +0800

    fourth

--- model.py
+++ model.py
@@ -3,10 +3,11 @@
 import numpy as np
 from dataset import get_data
 from data_preprocess import DataSet
+import matplotlib as pyplot
 
 # y_i = beta.T @ X_i
 class LinearRegression:
-    def __init__(self, X, Y, seed = 42) -> None:
+    def __init__(self, X, Y, seed = 123) -> None:
         np.random.seed(42)
         self.X = np.array(X)
         self.Y = np.array(Y)
@@ -73,15 +74,21 @@ class RBFKernelRegression:
         np.random.seed(42)
         self.X = np.array(X)
         self.Y = np.array(Y)
+        m, n = X.shape
         self.sigma = sigma
         self.Lambda = Lambda
         self.num = len(Y)
-        self.c = np.random.random(X.shape[0])
-        m, n = self.X.shape
-        X_2 = (np.sum(self.X**2, axis = 1) * np.ones((m, m)))
-        X_Y = self.X @ self.X.T
-        t = X_2 - 2 * X_Y + X_2.T
-        self.K = np.exp(-t / (2 * self.sigma**2))
+        self.c = np.random.random(m)
+        self.K = self.__Cal_K__(self.X, self.X)
+        
+    def __Cal_K__(self, X, Y):
+        n, p = X.shape
+        m, p = Y.shape
+        X_2 = (np.sum(X**2, axis = 1) * np.ones((m, n))).T
+        X_Y = X @ Y.T
+        Y_2 = (np.sum(Y**2, axis = 1) * np.ones((n, m)))
+        t = X_2 - 2 * X_Y + Y_2
+        return np.exp(-t / (2 * self.sigma**2)).T
     
     def loss(self):
         Y_predict = self.predict(self.X)
@@ -95,23 +102,64 @@ class RBFKernelRegression:
         return self.deri
         
     def analysis_fit(self):
-        self.beta = np.linalg.inv(self.K + self.Lambda * np.eye(len(self.c))) @ Y
+        self.c = np.linalg.inv(self.K + self.Lambda * np.eye(len(self.c))) @ self.Y
     
     def update(self, lr = 0.000002):
         self.c = self.c - lr * self.deri
     
-    def predict(self, X): 
+    def predict(self, X_): 
+        K = self.__Cal_K__(self.X, X_)
         return K @ self.c
 
-class LassRegression:
-    pass
-
+#l(beta) = 1/2 * (Y - X @ beta).T @ (Y - X @ beta) + Lambda * |beta|
+class LassoRegression:
+    def __init__(self, X, Y, Lambda = 0.01, seed = 123) -> None:
+        np.random.seed(42)
+        self.X = np.array(X)
+        self.Y = np.array(Y)
+        self.Lambda = Lambda
+        self.num = len(Y)
+        self.beta = np.random.random(X.shape[1])
+        
+    def loss(self):
+        Y_predict = self.predict(self.X)
+        self.loss_ = (0.5 * (self.Y - Y_predict).T @ (self.Y - Y_predict) + \
+            self.Lambda * np.linalg.norm(x= self.beta, ord= 1)) / self.num
+        return self.loss_
+    
+    def derivative(self):
+        Y_predict = self.predict(self.X)
+        self.deri = (self.X.T @ (Y_predict - self.Y) + self.Lambda * np.sign(self.beta)) / self.num
+        return self.deri
+    
+    def update(self, lr = 0.000002):
+        self.beta = self.beta - lr * self.deri
+    
+    def predict(self, X): 
+        return X @ self.beta
+    
+    def CoordinateDescent(self, a, b, interval = 100):
+        n, p = self.X.shape
+        delta = (b - a) / interval
+        for t in range(interval):
+            Lambda = 10**(b - t * delta)
+            for j in range(p):
+                beta = self.beta.copy()
+                beta[j] = 0
+                R = self.Y - self.X @ beta
+                Xj = self.X.T[j]
+                gamma = R @ Xj / (Xj.T @ Xj)
+                self.beta[j] = np.sign(gamma) * max(0, abs(gamma) - Lambda / (Xj.T @ Xj))
+    
 def sse_loss(Y_predict, Y_test):
     N = len(Y_test)
     sum = 0
     for (i, j) in zip(Y_predict, Y_test):
         sum += (i - j)**2
     return sum
+
+def polt_beta(beta):
+    
 
 if __name__ == "__main__":
     epochs = 1000000
@@ -120,16 +168,32 @@ if __name__ == "__main__":
     Data.normalize() # Data normalization
     X_train, Y_train = Data.train_set()
     X_test, Y_test = Data.test_set()
-    Kernel = RBFKernelRegression(X_train, Y_train)
+    
     # Linear = LinearRegression(X_train, Y_train)
-    # Ridge = RidgeRegression(X_train, Y_train, 90)
-    # Ridge.analysis_fit(X_train, Y_train)
+    Ridge = RidgeRegression(X_train, Y_train, 90)
+    Ridge.analysis_fit(X_train, Y_train)
+    
+    Kernel = RBFKernelRegression(X_train, Y_train, 5, 50)
+    Kernel.analysis_fit()
+    
+    Lasso = LassoRegression(X_train, Y_train, 60)
+    Lasso.CoordinateDescent(1.5, 0.01, 1000)
     # for epoch in range(epochs):
     #     loss = Linear.loss()
     #     deri = Linear.derivative()
     #     Linear.update()
     #     if(epoch%5000 == 0):
     #         print(f'Epoch {epoch}: loss = {loss}, derivative = {deri}')
+    
+    # for epoch in range(epochs):
+    #     loss = Lasso.loss()
+    #     deri = Lasso.derivative()
+    #     Lasso.update()
+    #     if(epoch%5000 == 0):
+    #         print(f'Epoch {epoch}: loss = {loss}, derivative = {deri}')
             
-    # print(sse_loss(Linear.predict(X_test), Y_test))
-    # print(sse_loss(Ridge.predict(X_test), Y_test))
+    # print(f'Linear: {sse_loss(Linear.predict(X_test), Y_test)}')
+    # print(f'Ridge: {sse_loss(Ridge.predict(X_test), Y_test)}')
+    # print(f'RBFKerner: {sse_loss(Kernel.predict(X_test), Y_test)}')
+
+    print(f'Lasso: {sse_loss(Lasso.predict(X_test), Y_test)}')
--- test.py
+++ test.py
@@ -4,8 +4,13 @@ from dataset import get_data
 
 X_train, X_test, Y_train, Y_test = get_data("./dataset/forestfires.csv")
 
-X = np.array([[1, 2, 3], [2, 2, 3], [5, 3, 2]])
+# X = np.array([[1, 2, 3], [2, 2, 3], [5, 3, 2]])
 
-print(np.ones((3, 3)))
-print(np.sum(X**2, axis = 1))
-print((np.sum(X**2, axis = 1) * np.ones((3,3))).T)
+# print(np.ones((4, 3)))
+# print(np.sum(X**2, axis = 1))
+# print((np.sum(X**2, axis = 1) * np.ones((4,3))))
+
+# beta = np.array([1, -1, -2, 2])
+# print(np.sign(beta))
+
+print(max(0, -1))
